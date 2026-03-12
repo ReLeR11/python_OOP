1bf5081905e82fca59cc83051f1cd69c221cdb02
MiLko <Milko82@students.softuni.bg>
2026-03-12 17:11:55 +0200

    Add solid lab

---
+++ solid/lab/03_ducks.py
@@ -0,0 +1,41 @@
+from abc import abstractmethod, ABC
+
+
+
+class BaseToy(ABC):
+    @staticmethod
+    def make_sound():
+        pass
+
+
+class RubberDuck(BaseToy):
+    @staticmethod
+    def make_sound():
+        return "Squeek"
+
+
+
+class RobotDuck(BaseToy):
+    HEIGHT = 50
+
+    def __init__(self):
+        self.height = 0
+
+    @staticmethod
+    def quack():
+        return 'Robotic quacking'
+
+    @staticmethod
+    def walk():
+        return 'Robotic walking'
+
+    def fly(self):
+        """can only fly to specific height but
+        when it reaches it starts landing automatically"""
+        if self.height == RobotDuck.HEIGHT:
+            self.land()
+        else:
+            self.height += 1
+
+    def land(self):
+        self.height = 0
